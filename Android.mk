839f7a08c0f42f26f78ab0e0b37fc0e87c8db912
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-02-24 20:48:05 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -10,3 +10,13 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),CPH1989)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+# health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
